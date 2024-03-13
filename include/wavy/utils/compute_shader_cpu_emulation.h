6f431adc132813aa70ff97f0802651df2edb259a
Sebastian Maisch <sebastian.maisch@googlemail.com>
2024-03-13 23:39:42 +0100

    Added first version of compute shader emulation.

---
+++ include/wavy/utils/compute_shader_cpu_emulation.h
@@ -0,0 +1,102 @@
+/**
+ * @file   compute_shader_cpu_emulation.h
+ * @author Sebastian Maisch <sebastian.maisch@googlemail.com>
+ * @date   2024.03.13
+ *
+ * @brief  Compute shader emulation on the CPU.
+ */
+
+#pragma once
+
+#include "utils/async_barrier_on_threadpool.h"
+
+#include <glm/vec3.hpp>
+#pragma warning(push)
+#pragma warning(disable : 5246)
+#include <mdspan>
+#pragma warning(pop)
+
+namespace wavy::utils {
+
+    using barrier_span_type = std::mdspan<async_barrier_on_threadpool, std::dextents<std::size_t, 3>>;
+    using shared_memory_span_type = std::mdspan<std::uint8_t, std::dextents<std::size_t, 4>>;
+    using task_span_type = std::mdspan<cppcoro::task<>, std::dextents<std::size_t, 3>>;
+
+    struct work_group_info
+    {
+        glm::uvec3 num_work_groups;
+        glm::uvec3 work_group_size;
+        glm::uvec3 work_group_id;
+        async_barrier_on_threadpool& barrier;
+        std::span<std::uint8_t> shared_memory;
+    };
+
+    cppcoro::task<> run_emulated_cs_kernel_on_thread_pool(cppcoro::static_thread_pool& tp, cppcoro::task<> kernel);
+    cppcoro::task<> wait_for_emulated_cs_tasks(std::vector<cppcoro::task<>> awaitables);
+
+    template<typename Pred>
+    void schedule_emulated_compute_shader_work_group(cppcoro::static_thread_pool& tp, task_span_type awaitables,
+                                                     const work_group_info& winfo, Pred kernel)
+    {
+        for (std::size_t liz = 0; liz < winfo.work_group_size.z; ++liz) {
+            for (std::size_t liy = 0; liy < winfo.work_group_size.y; ++liy) {
+                for (std::size_t lix = 0; lix < winfo.work_group_size.x; ++lix) {
+                    glm::uvec3 local_invocation_id{lix, liy, liz};
+                    glm::uvec3 global_invocation_id = winfo.work_group_id * winfo.work_group_size + local_invocation_id;
+                    unsigned int local_invocation_index =
+                        local_invocation_id.z * winfo.work_group_size.x * winfo.work_group_size.y
+                        + local_invocation_id.y * winfo.work_group_size.x + local_invocation_id.x;
+
+                    auto& awaitable = awaitables[std::array<std::size_t, 3>{
+                        {global_invocation_id.x, global_invocation_id.y, global_invocation_id.z}}];
+
+                    awaitable = run_emulated_cs_kernel_on_thread_pool(
+                        tp, kernel(winfo, local_invocation_id, global_invocation_id, local_invocation_index));
+                }
+            }
+        }
+    }
+
+    template<typename Pred>
+    void emulate_compute_shader(const glm::uvec3& work_groups, const glm::uvec3& work_group_size,
+                                std::size_t shared_memory_size, Pred kernel)
+    {
+        auto thread_pool = std::make_shared<cppcoro::static_thread_pool>();
+
+        std::size_t work_groups_linear = work_groups.x * work_groups.y * work_groups.z;
+        std::size_t work_group_size_linear = work_group_size.x * work_group_size.y * work_group_size.z;
+
+        std::vector<async_barrier_on_threadpool> barriers_linear(work_groups_linear);
+        std::vector<std::uint8_t> shared_memory_linear(work_groups_linear * shared_memory_size);
+        std::vector<cppcoro::task<>> awaitables_linear(work_groups_linear * work_group_size_linear);
+
+        barrier_span_type barriers(barriers_linear.data(), work_groups.x, work_groups.y, work_groups.z);
+        shared_memory_span_type shared_memory(shared_memory_linear.data(), shared_memory_size, work_groups.x,
+                                              work_groups.y, work_groups.z);
+        task_span_type awaitables(awaitables_linear.data(), work_groups.x * work_group_size.x,
+                                  work_groups.y * work_group_size.y, work_groups.z * work_group_size.z);
+
+        for (std::size_t wiz = 0; wiz < work_groups.z; ++wiz) {
+            for (std::size_t wiy = 0; wiy < work_groups.y; ++wiy) {
+                for (std::size_t wix = 0; wix < work_groups.x; ++wix) {
+                    glm::uvec3 work_group_id{wix, wiy, wiz};
+
+                    auto& barrier =
+                        barriers[std::array<std::size_t, 3>{{work_group_id.x, work_group_id.y, work_group_id.z}}];
+                    barrier.reset(thread_pool, static_cast<std::ptrdiff_t>(work_group_size_linear));
+                    work_group_info winfo{.num_work_groups = work_groups,
+                                          .work_group_size = work_group_size,
+                                          .work_group_id = work_group_id,
+                                          .barrier = barrier,
+                                          .shared_memory{&shared_memory[std::array<std::size_t, 4>{
+                                                             {0, work_group_id.x, work_group_id.y, work_group_id.z}}],
+                                                         shared_memory_size}};
+
+                    schedule_emulated_compute_shader_work_group(*thread_pool, awaitables, winfo, kernel);
+                }
+            }
+        }
+
+        cppcoro::sync_wait(wait_for_emulated_cs_tasks(awaitables));
+    }
+}
---
+++ src/wavy/utils/compute_shader_cpu_emulation.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file   compute_shader_cpu_emulation.cpp
+ * @author Sebastian Maisch <sebastian.maisch@googlemail.com>
+ * @date   2024.03.13
+ *
+ * @brief  Implementation of compute shader emulation on the CPU.
+ */
+
+#include "utils/compute_shader_cpu_emulation.h"
+#include <cppcoro/when_all.hpp>
+
+namespace wavy::utils {
+
+    cppcoro::task<> run_emulated_cs_kernel_on_thread_pool(cppcoro::static_thread_pool& tp, cppcoro::task<> kernel)
+    {
+        co_await tp.schedule();
+        co_await kernel;
+    }
+
+    cppcoro::task<> wait_for_emulated_cs_tasks(std::vector<cppcoro::task<>> awaitables)
+    {
+        co_await cppcoro::when_all(std::move(awaitables));
+    }
+
+}
